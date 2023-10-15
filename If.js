0b9b1c68f12fd0cfc4d1aac74d143daa854ca8e6
JuryFoil <mgarcia14@ucol.mx>
2023-10-14 20:56:29 -0600

    first commit

---
+++ If.js
@@ -0,0 +1,7 @@
+let num = prompt("Introduzca un numero...")
+
+if(num>0) {
+    console.log("el numero es positivo");
+} else {
+    console.log("el numero es negativo")
+};
---
+++ Switch.js
@@ -0,0 +1,21 @@
+let color = prompt("esciba una letra A, B o C")
+
+switch (color) {
+
+    case "A":
+    console.log("A de azul");
+    break;
+
+    case  "B":
+    console.log("B de Beige");
+    break;
+
+    case "C":
+    console.log("C de Cian");
+    break;
+
+    default:
+        console.log("No integrado");
+        break;
+
+}
